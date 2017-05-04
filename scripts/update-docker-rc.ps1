20182a06ae22befcedf39db656b0818d993699e2
Stefan Scherer <scherer_stefan@icloud.com>
2017-05-04 10:48:43 +0200

    Update rc3

--- scripts/update-docker-rc.ps1
+++ scripts/update-docker-rc.ps1
@@ -3,7 +3,7 @@ $ProgressPreference = 'SilentlyContinue'
 
 Write-Host "Stopping docker service"
 Stop-Service docker
-$version = "17.05.0-ce-rc1"
+$version = "17.05.0-ce-rc3"
 
 Write-Host "Downloading docker-$version.zip"
 $wc = New-Object net.webclient
