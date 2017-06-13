414e8e83c354fd8aeea1020110f641b887e7c289
Stefan Scherer <scherer_stefan@icloud.com>
2017-06-13 10:50:14 +0200

    Update 17.06.0-ce-rc2

--- scripts/update-docker-rc.ps1
+++ scripts/update-docker-rc.ps1
@@ -1,18 +1,22 @@
-$ErrorActionPreference = 'Stop'
-$ProgressPreference = 'SilentlyContinue'
-
 Write-Host "Stopping docker service"
 Stop-Service docker
-$version = "17.05.0-ce-rc3"
+$version = "17.06.0-ce-rc2"
 
 Write-Host "Downloading docker-$version.zip"
 $wc = New-Object net.webclient
-$wc.DownloadFile("https://test.docker.com/builds/Windows/x86_64/docker-$version.zip", "$env:TEMP\docker-$version.zip")
+$wc.DownloadFile("https://download.docker.com/win/static/test/x86_64/docker-$version-x86_64.zip", "$env:TEMP\docker-$version.zip")
 Write-Host "Extracting docker-$version.zip"
 Expand-Archive -Path "$env:TEMP\docker-$version.zip" -DestinationPath $env:ProgramFiles -Force
 Remove-Item "$env:TEMP\docker-$version.zip"
 
+Write-Host "Activating experimental features"
+$daemonJson = "$env:ProgramData\docker\config\daemon.json"
+$config = @{}
+if (Test-Path $daemonJson) {
+  $config = (Get-Content $daemonJson) -join "`n" | ConvertFrom-Json
+}
+$config = $config | Add-Member(@{ experimental = $true }) -Force -PassThru
+$config | ConvertTo-Json | Set-Content $daemonJson -Encoding Ascii
+
 Write-Host "Starting docker service"
 Start-Service docker
-
-docker version
