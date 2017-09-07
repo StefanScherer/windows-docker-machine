2fe8a2326f2677e9bcf5eeec6a608c59c825ac26
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-07 15:42:51 +0200

    Update Docker 17.09.0-ce-rc1

--- scripts/update-docker-rc.ps1
+++ scripts/update-docker-rc.ps1
@@ -1,10 +1,10 @@
 Write-Host "Stopping docker service"
 Stop-Service docker
-$version = "17.06.0-ce-rc3"
+$version = "17.09.0-ce-rc1"
 
 Write-Host "Downloading docker-$version.zip"
 $wc = New-Object net.webclient
-$wc.DownloadFile("https://download.docker.com/win/static/test/x86_64/docker-$version-x86_64.zip", "$env:TEMP\docker-$version.zip")
+$wc.DownloadFile("https://download.docker.com/win/static/test/x86_64/docker-$version.zip", "$env:TEMP\docker-$version.zip")
 Write-Host "Extracting docker-$version.zip"
 Expand-Archive -Path "$env:TEMP\docker-$version.zip" -DestinationPath $env:ProgramFiles -Force
 Remove-Item "$env:TEMP\docker-$version.zip"
